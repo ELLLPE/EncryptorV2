5212a95b1f047431252803f2a4701eb2f864e1d5
ELLLPE <elias.arvidsson08@gmail.com>
2026-09-22 11:09:34 +0200

    feat: :construction: Expanded CharConverter
    Expantion on the CharConverter function

--- app/src/main/java/encryptorV2/dataConverter/CharConverter.java
+++ app/src/main/java/encryptorV2/dataConverter/CharConverter.java
@@ -1,9 +1,28 @@
 package encryptorV2.dataConverter;
 
+import encryptorV2.dataIntegrator.characterCodec.CharacterCodecService;
+
 public class CharConverter {
 
-    public static char convertToInt(char c) {
-        return (char) c;
+    private static CharacterCodecService ccs = new CharacterCodecService();
+    private static String cc = ccs.getCharacterCodec();
+
+    public static int convertToInt(char c) {
+
+        int integer = cc.indexOf(c);
+
+        return integer;
+    }
+
+    public static int[] convertToInt(char[] chars) {
+
+        int[] integers = new int[chars.length];
+
+        for (int i = 0; i < chars.length; i++) {
+            integers[i] = convertToInt(chars[i]);
+        }
+
+        return integers;
     }
 
 }
